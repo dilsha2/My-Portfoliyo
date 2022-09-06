6b121d3da13efe658bef9b7b17a0f3a174d4e39f
dilsha2 <diliprasanna96@gmail.com>
2022-09-06 18:12:47 +0530

    Java script add

--- js/script.js
+++ js/script.js
@@ -1,27 +1,46 @@
-$(document).ready(function () {
-    $(window).scroll(function () {
-        if (this.scrollY > 20) {
-            $('.navbar').addClass("sticky");
-        } else {
-            $('.navbar').removeClass("sticky");
+$(document).ready(function(){
+
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('header').toggleClass('toggle');
+    });
+
+    $(window).on('scroll load',function(){
+
+        $('#menu').removeClass('fa-times');
+        $('header').removeClass('toggle');
+
+        if($(window).scrollTop() > 0){
+            $('.top').show();
+        }else{
+            $('.top').hide();
         }
-    })
-
-    /* toggle menu/ navbar script */
-
-    $('.menu-btn').click(function () {
-
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
 
     });
 
-    /*typing animation script*/
+    // smooth scrolling
+
+    $('a[href*="#"]').on('click',function(e){
+
+        e.preventDefault();
+
+        $('html, body').animate({
+
+                scrollTop : $($(this).attr('href')).offset().top,
+
+            },
+            500,
+            'linear'
+        );
+
+    });
+
     var typed = new Typed(".typing",{
         strings:["Full Stack Developer","WEB-DEVELOPER","BACK-END DEVELOPER","FRONT-END DEVELOPER","SOFTWARE DESIGNER","UI/UX DESIGNER"],
         typeSpeed:80,
         backSpeed:50,
         loop:true
+
     });
 
 });
