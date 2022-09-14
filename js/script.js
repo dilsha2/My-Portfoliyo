61341c39b090f04723c83a4b0f96ebead5ce80c0
dilsha2 <diliprasanna96@gmail.com>
2022-09-14 16:30:13 +0530

    java script

--- js/script.js
+++ js/script.js
@@ -42,3 +42,74 @@ $(document).ready(function(){
 
     });
 });
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+
+const container = document.querySelector(".container");
+const cards = document.querySelector(".cards");
+
+/* keep track of user's mouse down and up */
+let isPressedDown = false;
+/* x horizontal space of cursor from inner container */
+let cursorXSpace;
+
+container.addEventListener("mousedown", (e) => {
+    isPressedDown = true;
+    cursorXSpace = e.offsetX - cards.offsetLeft;
+    container.style.cursor = "grabbing";
+});
+
+container.addEventListener("mouseup", () => {
+    container.style.cursor = "grab";
+});
+
+window.addEventListener("mouseup", () => {
+    isPressedDown = false;
+});
+
+container.addEventListener("mousemove", (e) => {
+    if (!isPressedDown) return;
+    e.preventDefault();
+    cards.style.left = `${e.offsetX - cursorXSpace}px`;
+    boundCards();
+});
+
+function boundCards() {
+    const container_rect = container.getBoundingClientRect();
+    const cards_rect = cards.getBoundingClientRect();
+
+    if (parseInt(cards.style.left) > 0) {
+        cards.style.left = 0;
+    } else if (cards_rect.right < container_rect.right) {
+        cards.style.left = `-${cards_rect.width - container_rect.width}px`;
+    }
+}
