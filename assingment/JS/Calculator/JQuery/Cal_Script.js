f1ac4892873b430efefc4b678e1a5f256d9a5c2e
dilsha2 <diliprasanna96@gmail.com>
2022-09-25 20:04:00 +0530

    Succeessfully create calculator

--- assingment/JS/Calculator/JQuery/Cal_Script.js
+++ assingment/JS/Calculator/JQuery/Cal_Script.js
@@ -2,3 +2,14 @@ function display(val){
     document.getElementById('result').value += val
     return val;
 }
+
+function solve(){
+    let x = document.getElementById('result').value
+    let y = eval(x);
+    document.getElementById('result').value = y
+    return y
+}
+
+function clearScreen(){
+    document.getElementById("result").value = "";
+}
