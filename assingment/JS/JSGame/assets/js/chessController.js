9923ede3d50b63212fcaf29e4cb1ac94c8d96dc4
unknown <diliprasanna96@gmail.com>
2022-11-20 11:19:51 +0530

    add javascript

---
+++ assingment/JS/JSGame/assets/js/chessController.js
@@ -0,0 +1,22 @@
+var arrMovedWhitePawnList = [];
+var arrMovedBlackPawnList = [];
+
+var arrMovedWhiteBishopList = [];
+var arrMovedBlackBishopList = [];
+
+var arrMovedWhiteKnightList = [];
+var arrMovedBlackKnightList = [];
+
+var arrMovedWhiteCastleList = [];
+var arrMovedBlackCastleList = [];
+
+var blackQueen = null;
+var whiteQueen = null;
+
+var selectedPiece = null;
+var previousMovedPeice = null;
+
+
+
+
+
