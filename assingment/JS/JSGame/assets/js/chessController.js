d7f9b3ab9e2eb98b18305e04980fda170ddcfcad
unknown <diliprasanna96@gmail.com>
2022-11-29 19:38:28 +0530

    content updated

--- assingment/JS/JSGame/assets/js/chessController.js
+++ assingment/JS/JSGame/assets/js/chessController.js
@@ -503,3 +503,30 @@ function isObstaclesFound(obj, nextCell) {
     }
     return true;
 }
+
+function change_to_2d() {
+    $("#border-container").css("transform", "rotateX(0deg)");
+    $("#border-container").css("transition-duration","3s");
+}
+/*setTimeout(my_function,5000);*/
+
+function change_to_3d() {
+    $("#border-container").css("transform", "rotateX(45deg)");
+    $("#border-container").css("transition-duration","3s");
+
+}
+
+function rotate_to_black() {
+    $("#border-container").css("transform", "rotate(180deg)");
+    $(".col-md-1 cell").css("transform", "rotate(180deg)");
+    $(".col-md-1 cell").css("transform", "rotate(180deg)");
+    $("#border-container").css("transition-duration","3s");
+}
+
+function rotate_to_white() {
+    $("#border-container").css("transform", "rotate(0deg)");
+    $(".col-md-1 cell").css("transform", "rotate(0deg)");
+    $(".col-md-1 cell").css("transform", "rotate(0deg)");
+    $("#border-container").css("transition-duration","3s");
+}
+
